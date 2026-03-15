b0cf37b3800cfaa58275b02f01811403826519a1
DuongPhuocQuang <duongphuocquang133@gmail.com>
2026-03-15 16:30:39 +0700

    Bai 02: tinh tong va hien thi ket qua

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace generic_collection
 {
@@ -6,12 +8,40 @@ namespace generic_collection
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
             Console.WriteLine("SV: Dương Phước Quang - MSSV: 2415053122131");
+            Console.WriteLine("Bài 02: Nhập List<int> và tính tổng các phần tử\n");
 
-            // Ví dụ thêm 1 dòng để test
-            Console.WriteLine("Hello!");
+            List<int> ds = new List<int>();
 
-            Console.WriteLine("Nhấn Enter để kết thúc...");
+            Console.WriteLine("Nhập các số nguyên (cách nhau bởi khoảng trắng). Ví dụ: 1 2 3 4 5");
+            Console.Write("Nhập: ");
+            string input = Console.ReadLine() ?? "";
+
+            string[] parts = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string p in parts)
+            {
+                if (int.TryParse(p, out int n))
+                    ds.Add(n);
+            }
+
+            if (ds.Count == 0)
+            {
+                Console.WriteLine("\nBạn chưa nhập số hợp lệ nào!");
+            }
+            else
+            {
+                int tong = 0;
+                foreach (int x in ds) tong += x;
+
+                Console.WriteLine("\nDanh sách đã nhập: " + string.Join(" ", ds));
+                Console.WriteLine("Tổng các phần tử = " + tong);
+            }
+
+            Console.WriteLine("\nNhấn Enter để kết thúc...");
             Console.ReadLine();
         }
     }
