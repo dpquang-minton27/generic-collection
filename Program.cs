541f81af20e7e078ea733bc6fe40162b00281229
DuongPhuocQuang <duongphuocquang133@gmail.com>
2026-03-17 23:15:33 +0700

    Tạo List<int>, đảo ngược thứ tự phần tử.

--- Program.cs
+++ Program.cs
@@ -4,47 +4,36 @@ using System.Text;
 
 namespace generic_collection
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Tên: Dương Phước Quang");
+            Console.WriteLine("MSV: 2415053122131");
+            Console.WriteLine("Đề bài: Tạo List<int>, đảo ngược thứ tự phần tử.");
+            Console.WriteLine("------------------------------");
 
-            Console.WriteLine("SV: Dương Phước Quang - MSSV: 2415053122131");
-            Console.WriteLine("Bài 03: Nhập List<int> và tìm số lớn nhất\n");
+            List<int> numbers = new List<int> { 1, 36, 22, 55, 89, 46 };
 
-            List<int> ds = new List<int>();
-
-            Console.WriteLine("Nhập các số nguyên (cách nhau bởi khoảng trắng). Ví dụ: 3 7 1 9 5");
-            Console.Write("Nhập: ");
-            string input = Console.ReadLine() ?? "";
-
-            char[] tach = new[] { ' ', '\t' };
-            string[] parts = input.Split(tach, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string p in parts)
+            Console.WriteLine("Danh sách ban đầu:");
+            foreach (int n in numbers)
             {
-                if (int.TryParse(p, out int n))
-                    ds.Add(n);
+                Console.Write(n + " ");
             }
 
-            if (ds.Count == 0)
-            {
-                Console.WriteLine("\nBạn chưa nhập số hợp lệ nào!");
-            }
-            else
-            {
-                int max = ds[0];
-                foreach (int x in ds)
-                    if (x > max) max = x;
+            Console.WriteLine();
 
-                Console.WriteLine("\nDanh sách đã nhập: " + string.Join(" ", ds));
-                Console.WriteLine("Số lớn nhất = " + max);
+            numbers.Reverse();
+
+            Console.WriteLine("Danh sách sau khi đảo ngược:");
+            foreach (int n in numbers)
+            {
+                Console.Write(n + " ");
             }
 
-            Console.WriteLine("\nNhấn Enter để kết thúc...");
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
