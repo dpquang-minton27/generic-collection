ca6ecc9ffe4c16a153689386b1784c1b24e75630
DuongPhuocQuang <duongphuocquang133@gmail.com>
2026-03-18 00:04:11 +0700

    Tạo Dictionary<string, string>, lưu từ điển Anh - Việt và tra nghĩa

--- Program.cs
+++ Program.cs
@@ -4,47 +4,36 @@ using System.Text;
 
 namespace generic_collection
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Tên: Dương Phước Quang");
+            Console.WriteLine("MSV: 2415053122131");
+            Console.WriteLine("Đề bài: Tạo Dictionary<string, string>, lưu từ điển Anh - Việt và tra nghĩa.");
+            Console.WriteLine("------------------------------");
 
-            Console.WriteLine("SV: Dương Phước Quang - MSSV: 2415053122131");
-            Console.WriteLine("Bài 03: Nhập List<int> và tìm số lớn nhất\n");
+            Dictionary<string, string> dictionary = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            dictionary.Add("hello", "xin chào");
+            dictionary.Add("book", "quyển sách");
+            dictionary.Add("computer", "máy tính");
+            dictionary.Add("student", "sinh viên");
+            dictionary.Add("school", "trường học");
 
-            List<int> ds = new List<int>();
+            Console.Write("Nhập từ tiếng Anh cần tra: ");
+            string word = Console.ReadLine();
 
-            Console.WriteLine("Nhập các số nguyên (cách nhau bởi khoảng trắng). Ví dụ: 3 7 1 9 5");
-            Console.Write("Nhập: ");
-            string input = Console.ReadLine() ?? "";
-
-            char[] tach = new[] { ' ', '\t' };
-            string[] parts = input.Split(tach, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string p in parts)
+            if (!string.IsNullOrEmpty(word) && dictionary.ContainsKey(word))
             {
-                if (int.TryParse(p, out int n))
-                    ds.Add(n);
-            }
-
-            if (ds.Count == 0)
-            {
-                Console.WriteLine("\nBạn chưa nhập số hợp lệ nào!");
+                Console.WriteLine("Nghĩa tiếng Việt: " + dictionary[word]);
             }
             else
             {
-                int max = ds[0];
-                foreach (int x in ds)
-                    if (x > max) max = x;
-
-                Console.WriteLine("\nDanh sách đã nhập: " + string.Join(" ", ds));
-                Console.WriteLine("Số lớn nhất = " + max);
+                Console.WriteLine("Không tìm thấy từ trong từ điển.");
             }
 
-            Console.WriteLine("\nNhấn Enter để kết thúc...");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
